1035f46710b870c5ac415a184c8c2210bea56b87
wendywuuu860207 <51086110+wendywuuu860207@users.noreply.github.com>
2019-11-03 13:43:59 +0800

    Update 20191105_main.py

--- 20191105_main.py
+++ 20191105_main.py
@@ -65,3 +65,72 @@ def create_table(lable, C_in, C_mid, C_out):
 
     for c in C_out:
         result[c[0]] = find_truthtable(lable, c)
+       result[c[0]] = find_truthtable(lable, c)
+        
+    return result
+
+def find_truthtable(lable, c):
+    temp = []
+    
+    # 建立gate的input
+    temp.append([lable[c[1][0]][0], lable[c[1][1]][0]])
+    temp.append([lable[c[1][0]][0], lable[c[1][1]][1]])
+    temp.append([lable[c[1][0]][1], lable[c[1][1]][0]])
+    temp.append([lable[c[1][0]][1], lable[c[1][1]][1]])
+
+    # 建立gate的table
+    lableID = c[0] + _BITSIZE * 2
+    for i in range(4):
+        if i in c[2]:
+            temp[i] = [hash(str([encrypt(temp[i][0]), encrypt(temp[i][1]),\
+                                 lable[lableID][1]])), lable[lableID][1]]
+        else:
+            temp[i] = [hash(str([encrypt(temp[i][0]), encrypt(temp[i][1]),\
+                                 lable[lableID][0]])), lable[lableID][0]]
+
+    # 打亂table
+    random.shuffle(temp)
+
+    return temp
+
+# 計算hash
+def get_hash(I1, I2, O):
+    return hash(str([encrypt(I1), encrypt(I2), O]))
+
+# 加密
+def encrypt(s):
+  m = hashlib.md5()
+  m.update(s.encode('utf-8'))
+  return m.hexdigest()
+
+# 驗證答案
+def decide(a, b, t):
+    for i in range(4):
+        temp = get_hash(a, b, t[i][1])
+        if temp == t[i][0]:
+            print(t[i][1])
+            return t[i][1]
+
+
+# 計算結果  
+def calculate(values, C_in, C_mid, C_out, table, lable):
+
+    # input數量
+    size = _BITSIZE * 2
+    # 輸入的值加上gate輸出的值
+    values = values + ([0] * (len(C_in) + len(C_mid) + len(C_out)))
+    
+    # 完成 IN
+    for c in C_in:
+        print('GateID：' + str(c[0]) + ' --->', end=' ')
+        # 找出上面的值
+        a = values[c[1][0]]
+            
+        # 找出下面的值
+        b = values[c[1][1]]
+
+        # 算出輸出值
+        t = table[c[0]]
+        
+        # 放回去values
+        values[size + c[0]] = decide(a, b, t)
